e9d7215699a72fbea9937e0badc2de8f75060818
Jerry <85411418@qq.com>
2023-11-10 15:57:43 +0800

    add README.md

--- unzip_test.go
+++ unzip_test.go
@@ -13,13 +13,6 @@ var (
 
 func TestDecompressFileFromURL(t *testing.T) {
 	xlog.Level = xlog.DebugLevel
-	//zipUrl := "https://test.cdn.sunmi.com/OTA/15008833235745.zip"
-	//zipUrl := "https://test.cdn.sunmi.com/OTA/15052081553365.zip"
-	//zipUrl := "https://cdn.test.sunmi.com/temp/generalfile/hardware/47422d780a8c468aa20f86de9ee7cd59.zip"
-	//zipUrl := "https://cdn.test.sunmi.com/temp/generalfile/hardware/93f292c855cb49379f9b3dcfe99c90f9.zip"
-	//zipUrl := "https://test.cdn.sunmi.com/OTA%5C15331150971730.zip"
-	zipUrl := "https://cdn.test.sunmi.com/temp/generalfile/hardware/75f01ce5197e455aba98ef24aaab280d.zip"
-	DecompressFileFromURL(ctx, zipUrl, []string{"version.txt", "scatter.txt", "boot.img"}, "/Users/jerry/file")
-	//file, _ := SaveFileFromURL(ctx, zipUrl, []string{"version.txt"})
-	//fmt.Println(string(file["version.txt"]))
+	zipUrl := "https://pay.wechatpay.cn/wiki/doc/apiv3/wechatpay/download/Product_5.zip"
+	DecompressFileFromURL(ctx, zipUrl, []string{"Product/Qt5Core.dll", "Product/Qt5Gui.dll", "Product/Qt5Widgets.dll"}, "/Users/jerry/file")
 }
