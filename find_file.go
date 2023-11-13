8b51e11d2b481866819bdb47c42428361e6a7f99
Bo.Tang <1430505705@qq.com>
2023-11-13 13:04:20 +0800

    zip reader

--- find_file.go
+++ find_file.go
@@ -47,27 +47,3 @@ func findFiles(c context.Context, zipUrl string, bs []byte, files []string, getS
 	}
 	return efs, nil
 }
-
-func findFilePath(c context.Context, zipUrl string, bs []byte, getSize int64) (efs []*ExtractFile, err error) {
-	r, err := zip.NewReader(bytes.NewReader(bs), getSize)
-	if err != nil {
-		return nil, err
-	}
-	for _, file := range r.File {
-		//xlog.Infof("fileName: %s , method: %d , size: %d , offset: %d", file.Name, file.Method, file.CompressedSize64, file.HeaderOffset) //scatter.txt
-		// 收集所有文件信息
-		item := &ExtractFile{
-			FileName:         file.Name,
-			Method:           file.Method,
-			CompressedSize:   int64(file.CompressedSize64),
-			UncompressedSize: int64(file.UncompressedSize64),
-			HeaderOffset:     file.HeaderOffset,
-		}
-		// 获取下载RangeStart
-		lfh, _ := getLocalFileHead(c, zipUrl, item.FileName, item.HeaderOffset)
-		item.RangeStart = file.HeaderOffset + zip.FileHeaderLen + int64(lfh.FileNameLen+lfh.ExtraLen)
-		item.RangeEnd = item.RangeStart + item.CompressedSize - 1
-		efs = append(efs, item)
-	}
-	return efs, nil
-}
